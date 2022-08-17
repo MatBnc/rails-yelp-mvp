797545b27bb04264524895ab98a3c84c257264b1
matteobianchi <matteo.bianchi@hec.edu>
2022-08-17 14:52:34 +0200

    the app works but it's ugly af

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+surpriz = {name: 'Surpriz', address: '110 rue Oberkampf', phone_number: '0156985347', category:'french'}
+dumplingeuh = {name: 'Dumplingeuh', address: '108 rue Oberkampf', phone_number: '0143957681', category:'chinese'}
+mamma_mia = {name: 'Mamma mia', address: '92 rue Oberkampf', phone_number: '0128917634', category:'italian'}
+sushi_town = {name: 'Sushi town', address: '80 rue Oberkampf', phone_number: '0146957200', category:'japanese'}
+frites_mamen = {name: 'Frites mamen', address: '120 rue Oberkampf', phone_number: '0119734682', category:'belgian'}
+
+[surpriz, dumplingeuh, mamma_mia, sushi_town, frites_mamen].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
